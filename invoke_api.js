6c81942775d272715a53b52d7ec4709f317076da
ameer hamza <mianameer998@gmail.com>
2023-09-19 14:23:53 +0500

    update

--- invoke_api.js
+++ invoke_api.js
@@ -1,4 +1,3 @@
-// import axios from "axios";
 const axios = require("axios");
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
@@ -9,76 +8,40 @@ async function invokeApi({
   queryParams = {},
   postData = {},
 }) {
-  // check authenticity
-  // sing request
-
-  //console.log(base_uri + path, "API URL");
-
-  let reqObj = {
-    method: method,
+  const reqObj = {
+    method,
     url: path,
     headers: {...headers},
+    params: queryParams,
+    data: postData,
   };
 
-  //console.log("HEADER=>>",reqObj.headers)
-
-  if (method === "GET") {
-    reqObj["params"] = queryParams;
-  }
-
-  if (method === "POST") {
-    reqObj["data"] = postData;
-    reqObj["params"] = queryParams;
-    //console.log("params called");
-  }
-  if (method === "PUT") {
-    reqObj["data"] = postData;
-    reqObj["params"] = queryParams;
-    //console.log("params called");
-  }
-  if (method === "DELETE") {
-    reqObj["data"] = postData;
-    reqObj["params"] = queryParams;
-    //console.log("params called");
-  }
-
-  let results = undefined;
+  let results;
 
   try {
-    console.log("axios requestOBJ ==> ", reqObj);
+    // console.log("axios requestOBJ ==> ", reqObj);
     results = await axios(reqObj);
-    console.log(results, "API results");
+    // console.log(results, "API results");
     return results.data;
   } catch (error) {
-    console.log(error);
-    console.log(error.response.status, "error.response.data.message");
-    if (error.response.status === 401) {
+    // console.error(error);
+
+    if (error.response && error.response.status === 401) {
       // alert(reqObj.url)
       // localStorage.clear();
       // window.location.reload();
     }
+
+    const errorMessage =
+      error.response?.data?.message ||
+      "" ||
+      error.response?.statusText ||
+      "Something went wrong";
+
     return {
-      code: error.response.status,
-      message: error.response.data.message ? error.response.data.message : "",
+      code: error.response?.status || 500,
+      message: errorMessage,
     };
-
-    if (error.response && error.response.data && error.response.data.message) {
-      throw new Error(error.response.data.message);
-    } else if (
-      error.response &&
-      error.response.status &&
-      error.response.statusText
-    ) {
-      //console.log(error.response);
-      throw new Error(error.response.statusText);
-    } else {
-      //console.log(error);
-      throw new Error("Some thing went wrong");
-    }
-  }
-
-  if (results.status !== 200) {
-    throw new Error(await results.text());
   }
 }
 
